3b22d24e9e1cce30113c32055f26e6f2ead7479f
GWI1 <97545440+GWI1@users.noreply.github.com>
2022-02-21 13:44:35 -0500

    Delete MyArraySizeException.java

--- src/Les2/MyArraySizeException.java
+++
@@ -1,8 +0,0 @@
-package Les2;
-
-public class MyArraySizeException {
-    public static void arrayOutOfBonds(int i, int j) {
-        int num1[][] = new int[4][4];
-        num1[i][j] = 5;
-    }
-}
