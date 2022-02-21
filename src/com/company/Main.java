441ae2c836d375b940796092a15bfadcb3c43ed5
GWI <greenwayitinc@gmail.com>
2022-02-21 12:45:27 -0500

    Dz2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Member[] members = new Member[3];
-        members[0]=new Member("Sasha",2000,650);
-        members[1]=new Member("Dima",7000,250);
-        members[2]=new Member("Masha", 4000,700);
+        Member[] members = {new Member("Sasha",2000,650), new Member("Dima",7000,250), new Member("Masha", 4000,700)};
         Team team = new Team("Training", members);
 
         Course course = new Course(3000,600);
